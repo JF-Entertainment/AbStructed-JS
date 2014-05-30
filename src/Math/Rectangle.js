0f58dd64954c82c8f779a57eaa642caa235462b6
Jan F <jfmulti@gmail.com>
2014-05-30 20:50:18 +0200

    [FIX] collisionbox-position

--- src/Math/Rectangle.js
+++ src/Math/Rectangle.js
@@ -13,47 +13,25 @@ define( ["Engine/Math/Structure", "Engine/Math/Vector2"] , function(Structure, V
 		isColliding: function(Modifier1, Modifier2, Structure2) {
 			
 			
+			var r1 = {
+				left: this.Position.X + Modifier1.X - this.Size.X/2,
+				right: this.Position.X + Modifier1.X + this.Size.X/2,
+				top: this.Position.Y + Modifier1.Y - this.Size.Y/2,
+				bottom: this.Position.Y + Modifier1.Y + this.Size.Y/2
+			};
 			
-			var Collision =     !(Structure2.Position.X  + Modifier2.X > this.Position.X + this.Size.X + Modifier1.X || 
-           						Structure2.Position.X + Structure2.Size.X + Modifier2.X < this.Position.X + Modifier1.X|| 
-								Structure2.Position.Y + Modifier2.Y > this.Position.Y + this.Size.Y + Modifier1.Y ||
-           						Structure2.Position.Y + Structure2.Size.Y + Modifier2.Y < this.Position.Y + Modifier1.Y);
+			var r2 = {
+				left: Structure2.Position.X + Modifier2.X - Structure2.Size.X/2,
+				right: Structure2.Position.X + Modifier2.X + Structure2.Size.X/2,
+				top: Structure2.Position.Y + Modifier2.Y - Structure2.Size.Y/2,
+				bottom: Structure2.Position.Y + Modifier2.Y + Structure2.Size.Y/2
+			};
 			
-//			var deltaX, deltaY = 0;
-//			
-//			if (movedX > 0) {	//Moving Right
-//				var intersectX = (Structure2.Position.X + Modifier2.X) - (this.Position.X + Modifier1.X) - this.Size.X;
-//				
-//				console.log(intersectX);
-//				
-//				deltaX += intersectX;
-//			}
-//			
-//			
-//			if (movedX < 0) {	//Moving Left
-//				var intersectX = (Structure2.Position.X + Modifier2.X) +  Structure2.Size.X - (this.Position.X + Modifier1.X);
-//
-//				deltaX += intersectX;
-//			}
-//			
-//			
-//			if (movedY > 0) {	//Moving Down
-//				
-//				
-//				var intersectY = (Structure2.Position.Y + Modifier2.Y) - (this.Position.Y + Modifier1.Y) - this.Size.Y;
-//				
-//				if (Structure2.Position.Y + Modifier2.Y < this.Position.Y + this.Size.Y + Modifier1.Y - movedY) intersectY = 0;
-//				
-//				deltaY += intersectY;
-//			}
-//			
-//			
-//			if (movedY < 0) {	//Moving Up
-//				var intersectY = (Structure2.Position.Y + Modifier2.Y) +  Structure2.Size.Y - (this.Position.Y + Modifier1.Y);
-//
-//				deltaY += intersectY;
-//			}
-//			
+			var Collision = !(r2.left > r1.right || 
+							r2.right < r1.left || 
+           					r2.top > r1.bottom ||
+           					r2.bottom < r1.top);
+			
 //			
 //			
 
